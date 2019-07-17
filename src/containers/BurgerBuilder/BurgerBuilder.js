eea82d615189a3da90c50476f98822d71fe45599
Antonin <anto.roy79@gmail.com>
2019-07-17 19:20:41 +0200

    First try axios

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -6,7 +6,9 @@ import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 
-import axios from '../../../axios-orders';
+import axios from '../../axios-orders';
+
+
 const INGREDIENT_PRICES = {
     salad: 0.5,
     cheese: 0.4,
@@ -78,11 +80,20 @@ class BurgerBuilder extends Component {
         this.setState({purchasing : false});
     }
     purchaseContinueHandler = () => {
-        alert('You continue!');
         const order = {
-
+            "ingredients" : JSON.stringify(this.state.ingredients),
+            "price" : this.state.totalPrice,
+            "delivryMethod" : "fastest",
+            "customer" : "Antonin",
+            "adress" : {
+                "street" : "Testadress",
+                "zipcode" : "79340",
+                "country" : "France"
+            }
         }
-        axios.post('/blog_posts');
+        axios.post('commands',order)
+            .then(response => console.log(response))
+            .catch(error => console.log(error));
     }
 
     render () {
